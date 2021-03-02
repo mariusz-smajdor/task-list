f3e0d8a25583b04585a2b9fd8f1253cb03bdcad4
Mariusz Smajdor <smaidorr@gmail.com>
2021-03-02 15:41:32 +0100

    task rendering function with temporary test tasks

--- js/script.js
+++ js/script.js
@@ -1,6 +1,31 @@
 {
+  const tasks = [
+    { content: "test1", done: true },
+    { content: "test2", done: false },
+  ];
+
+  const render = () => {
+    let tasksToHTML = "";
+
+    for (const task of tasks) {
+      tasksToHTML += `
+        <li class="list__item">
+        <button class="list__button">${task.done ? "✖" : "✔"}</button>
+          <span class="list__content ${task.done ? "list__content--done" : ""}">
+            ${task.content}
+          </span>
+          <button class="list__button list__button--remove">🗑</button>
+        </li>
+      `;
+    }
+
+    document.querySelector(".js-list").innerHTML = tasksToHTML;
+  };
+
   const onFormSubmit = (event) => {
     event.preventDefault();
+
+    render();
   };
 
   const init = () => {
